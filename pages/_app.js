09962fe7a404deac87c649bacfff3890b22ea0a8
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-05 22:49:40 +0200

    Revert "2-3-4 of the first assignment."
    This reverts commit ae9738ce3d95ed102299e1eafe9202ecc507e9df.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import {config} from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,12 @@
+import React from 'react'
+import styles from '../styles/Login.module.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+import {
+    faUser,
+    faLock,
+} from "@fortawesome/free-solid-svg-icons";
+
 function Login() {
     const submit = async (e) => {
         e.preventDefault();
@@ -17,11 +26,39 @@ function Login() {
     }
 
     return (
-        <form onSubmit={submit}>
-            <input type="text" name="username" placeholder="Username" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <button type="submit"> Submit </button>
-        </form>
+        <div class={styles.background}>
+            <div class={styles.container}>
+                <div class={styles.screen}>
+                    <div class={styles.content}>
+                        <form class={styles.login} onSubmit={submit}>
+                            <div class={styles.logn_field}>
+                                {/* Dont work for some reason */}
+                                <i class={styles.login_icon_one}>
+                                    <FontAwesomeIcon icon={faUser}/></i>
+                                <input type="text" class={styles.login_input} name="username" placeholder="Username" required />
+                            </div>
+                            <div class="login_field">
+                                {/* Dont work for some reason */}
+                                <i class={styles.login_icon_two}><FontAwesomeIcon icon={faLock} /></i>
+                                <input type="password" class={styles.login_input} name="password" placeholder="Password" required />
+                            </div>
+                            <button class={styles.login_submit} type="submit">
+                                <span class={styles.button_text}>Log in now</span>
+                            </button>
+                            <button class={styles.signup_submit}>
+                                <span class={styles.button_text}>Sign up</span>
+                            </button>
+                        </form>
+                    </div>
+                    <div class={styles.shape_backgrounds}>
+                        <span class={`${styles.shape_rotation} ${styles.shape_four}`}></span>
+                        <span class={`${styles.shape_rotation} ${styles.shape_three}`}></span>
+                        <span class={`${styles.shape_rotation} ${styles.shape_two}`}></span>
+                        <span class={`${styles.shape_rotation} ${styles.shape_one}`}></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
