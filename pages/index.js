c0b0183652d931b1a6afd0bbeed26f7b2e936f66
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-16 11:52:21 +0200

    s

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import styles from '../styles/index.module.css'
+
 export async function getServerSideProps(context) {
     let res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/User', {
         credentials: 'include',
@@ -15,7 +17,22 @@ export async function getServerSideProps(context) {
 
 export default function Home(props) {
     return (
-        <div>
+        <div className={styles.background}>
+            <div>
+                
+            </div>
+            <div className={styles.text_background}>
+                <h1 className={styles.main_title}>Songs <br/> Just <span className={styles.four_highlight}>4</span> You</h1>
+                <button className={styles.button_login} type="button">
+                        <span className={styles.button_text}>Get Started</span>
+                </button>
+            </div>
+        </div>
+        
+        
+        
+        
+        /*<div>
             <h1>Home</h1>
 
             {props.user ? (
@@ -23,6 +40,6 @@ export default function Home(props) {
             ) : (
                 <p> Welcome guest </p>
             )}
-        </div>
+        </div>*/
     )
 }
