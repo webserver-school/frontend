17268b9176c25c8f179af425140c50239a6ac620
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-21 19:25:49 +0200

    Created the base for index page, got tailwind colors to work as well

--- pages/index.js
+++ pages/index.js
@@ -18,19 +18,21 @@ export async function getServerSideProps(context) {
 
 export default function Home(props) {
     return (
-        <div className={styles.background}>
-            <div className={styles.text_background}>
-                <h1 className={styles.main_title}>Songs <br /> Just <span className={styles.four_highlight}>4</span> You</h1>
+        <div className="w-full min-h-screen flex overflow-hidden bg-black">
+            <div className="flex flex-row justify-between z-20">
+                <div className="container flex flex-col my-auto ml-16">
+                    <h1 className="text-8xl font-bold pb-10 leading-tight text-white">Songs <br /> Just <span className={styles.four_highlight}>4</span> You</h1>
 
-                {props.user ? (
-                    <p> Welcome {props.user.displayName} </p>
-                ) : (
-                    <Link href="/Login">
-                        <button className = {styles.button_login} type="button">
-                            <span className={styles.button_text}>Get Started</span>
-                        </button>
-                    </Link>
-                )}
+                    {props.user ? (
+                        <p> Welcome {props.user.displayName} </p>
+                    ) : (
+                        <Link href="/Login">
+                            <button className = "bg-purple text-2xl py-4 uppercase text-white rounded-2xl mr-6" type="button">
+                                <span>Get Started</span>
+                            </button>
+                        </Link>
+                    )}
+                </div>
             </div>
             <img src="/Polygon 6.svg" alt="icon_triangle" id={styles.svg1}></img>
             <img src="/Polygon 6.svg" alt="icon_triangle" id={styles.svg2}></img>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: colors.black,
+        purple: "#5200FF",
+        red: colors.red,
+      }
+    },
   },
   plugins: [],
 }
